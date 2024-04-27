6d8981ec25a70ec423b163ca8b5fc860b74b840b
nkhou <najat.khoudrouf75@gmail.com>
2024-04-27 18:07:13 +0100

    try to fix some error in script

--- src/requirements/mariadb/conf/script.sh
+++ src/requirements/mariadb/conf/script.sh
@@ -2,10 +2,10 @@
 
 service mysql start
 
-mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
-# mysql -e "CREAT USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-# mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY `${SQL_PASSWORD}`;"
-# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-# mysql -e "FLUSH PRIVILEGES;"
-# mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -u "$MYSQL_ROOT" -p "$MYSQL_ROOT_PASSWORD" -e "ALTER USER \`${MYSQL_ROOT}\`@'localhost' IDENTIFIED BY \`${MYSQL_ROOT_PASSWORD}\`;"
+mysql -e "FLUSH PRIVILEGES;"
+# mysqladmin -u $MYSQL_ROOT -p $MYSQL_ROOT_PASSWORD shutdown
 # exec mysqld_safe
