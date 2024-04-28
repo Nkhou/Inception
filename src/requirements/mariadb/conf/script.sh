8615deaef4140ea9fb1e8e97d944fc50327f256f
nkhou <najat.khoudrouf75@gmail.com>
2024-04-28 19:32:04 +0100

    .env

--- src/requirements/mariadb/conf/script.sh
+++ src/requirements/mariadb/conf/script.sh
@@ -4,8 +4,7 @@ service mysql start
 
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -u "$MYSQL_ROOT" -p "$MYSQL_ROOT_PASSWORD" -e "ALTER USER \`${MYSQL_ROOT}\`@'localhost' IDENTIFIED BY \`${MYSQL_ROOT_PASSWORD}\`;"
-mysql -e "FLUSH PRIVILEGES;"
-# mysqladmin -u $MYSQL_ROOT -p $MYSQL_ROOT_PASSWORD shutdown
+mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; FLUSH PRIVILEGES;"
+mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
 # exec mysqld_safe
