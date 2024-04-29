c5568f3836547dd90f05edf6f2b62f5fb16526cf
nkhou <najat.khoudrouf75@gmail.com>
2024-04-29 19:03:58 +0100

    fix mariadb

--- src/requirements/mariadb/conf/script.sh
+++ src/requirements/mariadb/conf/script.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-service mysql start
+service mariadb start
+
+# cat 2> /dev/null
 
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; FLUSH PRIVILEGES;"
-mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
-# exec mysqld_safe
+mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; "
+
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+
+exec mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
